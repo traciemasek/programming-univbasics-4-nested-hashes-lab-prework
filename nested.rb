8910f4bd9f52516d93b3cdf835a698768b49c72b
Tracie Masek <traciemasek@gmail.com>
2019-06-21 11:03:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,8 +20,6 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	programmer_hash[:alan_kay][:known_for]
-	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +35,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
